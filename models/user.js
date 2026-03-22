93511bd4bc5199ad1488ed6e71a43ff92504c027
Brian Rodríguez <brianrp19@gmail.com>
2026-03-22 16:37:04 -0600

    feat: Add initial user model and routes for API health check and user retrieval by cedula.

---
+++ models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    cedula: { type: Number, required: true },
+    nombre: String
+})
+
+module.exports = mongoose.model('user', userSchema, 'Cedulas');
